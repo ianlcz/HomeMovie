8d1c59743fa8644e73b736cd5ede3747a1376e04
Yann LE COZ <ylcoz@icloud.com>
2022-09-21 22:23:40 +0200

    Set Dark Mode of Home page

--- src/components/TitlePage.js
+++
@@ -1,8 +0,0 @@
-const TitlePage = () => (
-  <div className="flex flex-row items-baseline justify-center w-full text-center mx-auto">
-    <h1 className="font-semibold text-2xl text-blue-50 mr-2">HomeMovie</h1>
-    <p className="font-light text-blue-50 ">by ianlcz</p>
-  </div>
-);
-
-export default TitlePage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import SearchBar from "../components/SearchBar";
 
 const Home = () => (
-  <div className="flex flex-col w-full bg-blue-50 min-h-screen">
+  <div className="flex flex-col w-full bg-blue-50 dark:bg-slate-800 min-h-screen">
     <SearchBar />
   </div>
 );
