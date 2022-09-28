dd959bfb3793ae3ae4417668c51a2facd428ed76
Yann Le Coz <ylcoz@icloud.com>
2022-09-28 22:29:56 +0200

    Verify provider flatrate and set dark mode of Actions

--- src/components/Actions.js
+++ src/components/Actions.js
@@ -18,7 +18,7 @@ const Actions = ({ children }) => {
         <div className="flex flex-row items-center lg:justify-around w-full lg:w-4/5 lg:mx-auto">
           <a
             href="/movies/new"
-            className="flex items-center w-max mx-auto lg:mx-0 px-2 py-1 text-green-600 hover:text-green-50 border border-green-500 hover:border-green-400 bg-green-50 hover:bg-green-400 rounded-full shadow"
+            className="flex items-center w-max mx-auto lg:mx-0 px-2 py-1 text-green-600 hover:text-green-50 border border-green-500 hover:border-green-400 bg-green-50 hover:bg-green-400 rounded-full shadow transition-all duration-300 ease-in-out"
           >
             <IoAddCircle className="w-5 h-5" />
             <span className="ml-2 text-sm">Ajouter un nouveau film</span>
@@ -29,7 +29,7 @@ const Actions = ({ children }) => {
               logout();
               window.location.reload(false);
             }}
-            className="flex items-center w-max mx-auto lg:mx-0 mt-0 px-2 py-1 text-blue-50 bg-gradient-to-br from-blue-800 to-blue-400 border-2 border-blue-50 hover:border-blue-300 rounded-full shadow-inner"
+            className="flex items-center w-max mx-auto lg:mx-0 mt-0 px-2 py-1 text-blue-50 bg-gradient-to-br from-blue-800 to-blue-400 border-2 border-blue-50 hover:border-blue-300 rounded-full shadow-inner transition-all duration-500 ease-in-out"
           >
             <IoExit className="w-5 h-5" />
             <span className="ml-2 text-sm">Me déconnecter</span>
@@ -41,7 +41,7 @@ const Actions = ({ children }) => {
             href={`/movies/edit/${children.ref}/${encodeURIComponent(
               children.title,
             )}`}
-            className="flex items-center w-max mr-5 px-2 py-1 text-gray-400 hover:text-gray-50 border border-gray-400 hover:border-gray-400 bg-gray-50 hover:bg-gray-400 rounded-full shadow"
+            className="flex items-center w-max mr-5 px-2 py-1 text-gray-400 hover:text-gray-50 border border-gray-400 hover:border-gray-400 bg-gray-50 dark:bg-gray-500/30 hover:bg-gray-400 rounded-full shadow transition-all duration-300 ease-in-out"
           >
             <IoSettings className="w-4 h-4" />
             <span className="ml-2 text-xs">Modifier</span>
@@ -51,7 +51,7 @@ const Actions = ({ children }) => {
             href={`/movies/delete/${children.ref}/${encodeURIComponent(
               children.title,
             )}`}
-            className="flex items-center w-max px-2 py-1 text-red-600 hover:text-red-50 border border-red-500 hover:border-red-400 bg-red-50 hover:bg-red-400 rounded-full shadow"
+            className="flex items-center w-max px-2 py-1 text-red-600 hover:text-red-50 border border-red-500 hover:border-red-400 bg-red-50 dark:bg-red-500/20 hover:bg-red-400 rounded-full shadow transition-all duration-300 ease-in-out"
           >
             <IoRemoveCircle className="w-4 h-4" />
             <span className="ml-2 text-xs">Retirer</span>
