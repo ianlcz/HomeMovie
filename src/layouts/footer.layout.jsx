13b6b32be1c350766a3814029e7515c1c858e283
Yann Le Coz <ylcoz@icloud.com>
2023-04-29 01:19:36 +0200

    Update footer.layout.jsx

--- src/layouts/footer.layout.jsx
+++ src/layouts/footer.layout.jsx
@@ -1,7 +1,6 @@
 const Footer = () => (
   <footer className="flex flex-col w-max mx-auto bottom-0 mt-auto px-2 py-0.5 bg-blue-50 justify-center items-center text-white text-xs font-light bg-gradient-to-tr from-blue-800 to-blue-400 rounded-t-lg shadow-inner">
-    <p className="font-semibold">License MIT</p>
-    <div className="flex flex-row mt-0.5">
+    <div className="flex flex-row">
       <p className="mr-1">Copyright &copy; 2021 - {new Date().getFullYear()}</p>
       <a
         href="https://github.com/ianlcz"
@@ -9,7 +8,7 @@ const Footer = () => (
         rel="noreferrer"
         title="Voir le profil GitHub"
       >
-        Yann Le Coz
+        ianlcz
       </a>
     </div>
   </footer>
