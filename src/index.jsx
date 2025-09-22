121c08ca324f049f643c9b868cc8db2126dc5f7b
Yann Le Coz <ylcoz@icloud.com>
2025-09-22 21:06:41 +0200

    Remove meta theme-color

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { Helmet } from "react-helmet";
 import "./index.css";
 import App from "./App";
 import Providers from "./contexts/Providers";
@@ -10,14 +9,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Providers>
-      <Helmet
-        meta={[
-          {
-            name: `theme-color`,
-            content: "#1e40af",
-          },
-        ]}
-      ></Helmet>
       <App />
     </Providers>
   </BrowserRouter>,
